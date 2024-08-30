b84dd8d6aba4a76da062c955cf4656e4bae8f225
Turamarth14 <felixheinrich@freenet.de>
2024-08-30 11:33:11 +0200

    Fixing errors with S3 methods

--- R/summary.opt_prediction_object.R
+++ R/summary.opt_prediction_object.R
@@ -1,35 +1,35 @@
 #' @export
 
-summary.opt_prediction_object = function(optRF_object){
+summary.opt_prediction_object = function(object, ...){
 
   # If the opt_prediction function could give expectations for the RF_stability, summarise those
-  if(!is.null(optRF_object$expected_RF_stability)){
-    if(nrow(optRF_object$expected_RF_stability) == 4){
-      cat("Recommended number of trees: ", optRF_object$recommendation, "\n",
-          "Expected prediction stability: ", optRF_object$expected_RF_stability[2], "\n",
-          "Expected selection stability: ", optRF_object$expected_RF_stability[3], "\n",
-          "Expected run time (sec): ", optRF_object$expected_RF_stability[4], sep = "")
+  if(!is.null(object$expected_RF_stability)){
+    if(nrow(object$expected_RF_stability) == 4){
+      cat("Recommended number of trees: ", object$recommendation, "\n",
+          "Expected prediction stability: ", object$expected_RF_stability[2], "\n",
+          "Expected selection stability: ", object$expected_RF_stability[3], "\n",
+          "Expected run time (sec): ", object$expected_RF_stability[4], sep = "")
     }
-    if(nrow(optRF_object$expected_RF_stability) == 3){
-      if(row.names(optRF_object$expected_RF_stability)[2] =="Prediction_stability"){
-        cat("Recommended number of trees: ", optRF_object$recommendation, "\n",
-            "Expected prediction stability: ", optRF_object$expected_RF_stability[2], "\n",
-            "Expected run time (sec): ", optRF_object$expected_RF_stability[3], "\n", sep = "")
+    if(nrow(object$expected_RF_stability) == 3){
+      if(row.names(object$expected_RF_stability)[2] =="Prediction_stability"){
+        cat("Recommended number of trees: ", object$recommendation, "\n",
+            "Expected prediction stability: ", object$expected_RF_stability[2], "\n",
+            "Expected run time (sec): ", object$expected_RF_stability[3], "\n", sep = "")
 
       }
-      if(row.names(optRF_object$expected_RF_stability)[2] =="Selection_stability"){
-        cat("Recommended number of trees: ", optRF_object$recommendation, "\n",
-            "Expected selection stability: ", optRF_object$expected_RF_stability[2], "\n",
-            "Expected run time (sec): ", optRF_object$expected_RF_stability[3], "\n", sep = "")
+      if(row.names(object$expected_RF_stability)[2] =="Selection_stability"){
+        cat("Recommended number of trees: ", object$recommendation, "\n",
+            "Expected selection stability: ", object$expected_RF_stability[2], "\n",
+            "Expected run time (sec): ", object$expected_RF_stability[3], "\n", sep = "")
       }
     }
   }
 
   # if the opt_prediction function could not give expectations for the RF_stability, summarise the result table
   else{
-    if(!is.null(optRF_object$result.table)){
+    if(!is.null(object$result.table)){
       cat("Result of the opt_prediction function: \n")
-      return(optRF_object$result.table)
+      return(object$result.table)
     }
     else{
       stop("Result of the opt_prediction function cannot be summarised.\n Did an error occur while running opt_prediction? \n")
