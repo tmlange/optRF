2c9743aa68788229af74e5a4900ff3dbe3c30c4f
FelixHeinrich <felix.heinrich@uni-goettingen.de>
2025-05-08 11:51:22 +0200

    Fixing bugs with the plot functions

--- R/plot.opt_importance_object.R
+++ R/plot.opt_importance_object.R
@@ -6,7 +6,7 @@ plot.opt_importance_object = function(x, ...){
   max.plot = round(max(x$result.table$num.trees_values)*1.1)
   plot_seq = seq(0, max.plot, 1)
 
-  plot(x$result.table$VI.stability ~ x$result.table$num.trees_values,
+  plot(x$result.table$VI_stability ~ x$result.table$num.trees_values,
        main='Relationship between\n variable importance stability and number of trees',
        ylab="Variable importance stability", xlab="number of trees", ...)
   points(TwoPLmodel(plot_seq, x$model.parameters[1,1], x$model.parameters[1,2]) ~ plot_seq,
