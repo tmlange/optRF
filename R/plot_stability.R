fda0b15220e3a0bd1814222abfbcd5990415adf1
FelixHeinrich <felix.heinrich@uni-goettingen.de>
2026-01-06 16:49:56 +0100

    Shortening the plot_stability function by using the get_target_measure helper function

--- R/plot_stability.R
+++ R/plot_stability.R
@@ -22,105 +22,61 @@
 #' }
 #'
 #' @export
-#' @importFrom methods is
-#' @importFrom grDevices rgb
-#' @importFrom graphics abline points
 
 plot_stability = function(optRF_object, measure = c("selection","importance","prediction"),
                           from = 0, to = 100000, add_recommendation = TRUE,
                           add = FALSE, ...){
 
   # Check if the correct object was inserted
-  if(!(is(optRF_object, "opt_prediction_object")) & !(is(optRF_object, "opt_importance_object"))){
+  if(!inherits(optRF_object, c("opt_prediction_object", "opt_importance_object"))){
     stop("Invalid object was inserted. The inserted object must be the result from the opt_prediction or opt_importance function.")
   }
 
   # Check value of measure
   measure = match.arg(measure)
+  is_pred = inherits(optRF_object, "opt_prediction_object")
+  target_measure = get_target_measure(measure, is_pred)
 
-  visualiseStability = function(param1, param2, ...){
-    if(add == FALSE){
-      plot(TwoPLmodel(plot_seq, param1, param2) ~ plot_seq,
-           type="l", ylab="Random forest stability", xlab="number of trees", main="Relationship between\n random forest stability and number of trees",
-           cex.axis=1.2, cex.lab=1.2, cex.main=1.2, lwd=1.2, ...)
-    }
-    else{
-      points(TwoPLmodel(plot_seq, param1, param2) ~ plot_seq,
-             type="l", lwd=1.2, ...)
-    }
-    if(add_recommendation == TRUE & !is.null(optRF_object$recommendation)){
-      abline(v = optRF_object$recommendation, col=rgb(1, 0, 0, 0.5))
-      abline(h = TwoPLmodel(optRF_object$recommendation, param1, param2),
-             col=rgb(1, 0, 0, 0.5))
-    }
+  # Check values of from and to
+  if (!is.numeric(from) || length(from) != 1 || from < 1) {
+    stop("Invalid input: 'from' must be a single positive number.")
   }
-
+  if (!is.numeric(to) || length(to) != 1 || to <= from) {
+    stop("Invalid input: 'to' must be a single positive number greater than the 'from' value.")
+  }
+  
+  model_params = optRF_object$model.parameters
+  
+  # If no model could be produced, give an error message
+  if(is.null(model_params) || nrow(model_params) == 0){ 
+    func_name = ifelse(inherits(optRF_object, "opt_prediction_object"), "opt_prediction", "opt_importance")
+    stop(paste0("The function ", func_name, " could not model the relationship between the number of trees and the stability."))
+  }
+  
+  # Check if model for selected stability measure exists
+  if(target_measure %in% rownames(model_params)){
+    use_measure = target_measure
+  } else{
+    # Give a warning if the non-selected stability is used for estimation
+    use_measure = rownames(model_params)[1]
+    warning(paste0("The optimal number of trees could not be estimated with the requested '", measure, 
+                   "' stability. Plotting was instead performed using the '", 
+                   gsub("_", "' ", use_measure), "."))
+  }
+  
+  # Calculate values and plot them
   plot_seq = seq(from, to, 1)
-
-  # If the object is an opt_prediction_object
-  if(is(optRF_object, "opt_prediction_object")){
-    # If the measure was set to be importance, this will not work
-    if(measure == "importance"){
-      stop("The variable importance stability cannot be plotted with an object created with the function opt_prediction.\nPlease set the measure argument to either \"prediction\" or \"selection\". \n")
-    }
-    # If no model could be produced, give an error message
-    if(nrow(optRF_object$model.parameters) == 0){
-      stop("The function opt_prediction could not model the relationship between the number of trees and prediction or selection stability.\n")
-    }
-    # If the measure was set to be prediction, plot the prediction stability
-    if(measure == "prediction"){
-      if(nrow(optRF_object$model.parameters) == 2 || # If a model for prediction and selection could be produced, plotting is not a problem
-         (nrow(optRF_object$model.parameters) == 1 && row.names(optRF_object$model.parameters) == "Prediction_stability")){ # If only one model could be produced, it must be the prediction model
-        visualiseStability(optRF_object$model.parameters[1,1], optRF_object$model.parameters[1,2], ...)
-      }
-      else{
-        stop("The function opt_prediction could not model the relationship between the number of trees and the prediction stability.\n")
-      }
-    }
-    else{ # If the measure was set to be selection, plot the selection stability
-      if(nrow(optRF_object$model.parameters) == 2){ # If a model for prediction and selection could be produced, plotting is not a problem
-        visualiseStability(optRF_object$model.parameters[2,1], optRF_object$model.parameters[2,2], ...)
-      }
-      else{
-        if(row.names(optRF_object$model.parameters) == "Selection_stability"){ # If only one model could be produced, it must be the selection model
-          visualiseStability(optRF_object$model.parameters[1,1], optRF_object$model.parameters[1,2], ...)
-        }
-        else{
-          stop("The function opt_prediction could not model the relationship between number of trees and the selection stability.\n")
-        }
-      }
-    }
+  stability_values = TwoPLmodel(plot_seq, model_params[use_measure,1], model_params[use_measure,2])
+  
+  if(add == FALSE){
+    plot(stability_values ~ plot_seq,
+         type = "l", ylab = paste0("RF ", gsub("_", " ", use_measure)), xlab = "Number of trees", main = paste0("Relationship between\n RF ", gsub("_", " ", use_measure)," and number of trees"), ...)
+  } else{
+    graphics::lines(stability_values ~ plot_seq, ...)
   }
-  else{ # If the object is an opt_importance_object
-    # If the measure was set to be prediction, this will not work
-    if(measure == "prediction"){
-      stop("The prediction stability cannot be plotted with an object created with the function opt_importance.\nPlease set the measure argument to either \"importance\" or \"selection\". \n")
-    }
-    if(nrow(optRF_object$model.parameters) == 0){ # If no model could be produced, give an error message
-      stop("The function opt_importance could not model the relationship between number of trees and variable importance or selection stability.\n")
-    }
-    # If the measure was set to be importance, plot the importance stability
-    if(measure == "importance"){
-      if(nrow(optRF_object$model.parameters) == 2 || # If a model for VI and selection could be produced, plotting is not a problem
-         (nrow(optRF_object$model.parameters) == 1 && row.names(optRF_object$model.parameters) == "VI_stability")){ # If only one model could be produced, it must be the VI model
-        visualiseStability(optRF_object$model.parameters[1,1], optRF_object$model.parameters[1,2], ...)
-      }
-      else{
-        stop("The function opt_importance could not model the relationship between number of trees and the variable importance stability.\n")
-      }
-    }
-    else{ # If the measure was set to be selection, plot the selection stability
-      if(nrow(optRF_object$model.parameters) == 2 ){ # If a model for VI and selection could be produced, plotting is not a problem
-        visualiseStability(optRF_object$model.parameters[2,1], optRF_object$model.parameters[2,2], ...)
-      }
-      else{
-        if(row.names(optRF_object$model.parameters) == "Selection_stability"){ # If only one model could be produced, it must be the selection model
-          visualiseStability(optRF_object$model.parameters[1,1], optRF_object$model.parameters[1,2], ...)
-        }
-        else{
-          stop("The function opt_importance could not model the relationship between number of trees and the selection stability.\n")
-        }
-      }
-    }
+  if(add_recommendation == TRUE && !is.null(optRF_object$recommendation)){
+    graphics::abline(v = optRF_object$recommendation, col=grDevices::rgb(1, 0, 0, 0.5))
+    graphics::abline(h = TwoPLmodel(optRF_object$recommendation, model_params[use_measure,1], model_params[use_measure,2]),
+           col=grDevices::rgb(1, 0, 0, 0.5))
   }
 }
