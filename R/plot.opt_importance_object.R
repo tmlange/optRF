277946b010ea0c64a19788578755e9fe4d7fcd49
FelixHeinrich <felix.heinrich@uni-goettingen.de>
2026-01-06 11:45:54 +0100

    Renamed variable and changed graphics function in plot.opt_*_object functions

--- R/plot.opt_importance_object.R
+++ R/plot.opt_importance_object.R
@@ -1,14 +1,13 @@
 #' @export
-#' @importFrom graphics points
 
 plot.opt_importance_object = function(x, ...){
 
-  max.plot = round(max(x$result.table$num.trees_values)*1.1)
-  plot_seq = seq(0, max.plot, 1)
+  max_plot = round(max(x$result.table$num.trees_values)*1.1)
+  plot_seq = seq(0, max_plot, 1)
 
   plot(x$result.table$VI_stability ~ x$result.table$num.trees_values,
        main='Relationship between\n variable importance stability and number of trees',
        ylab="Variable importance stability", xlab="number of trees", ...)
-  points(TwoPLmodel(plot_seq, x$model.parameters[1,1], x$model.parameters[1,2]) ~ plot_seq,
-         type="l", col="navyblue", lwd=3)
+  graphics::lines(TwoPLmodel(plot_seq, x$model.parameters[1,1], x$model.parameters[1,2]) ~ plot_seq,
+         col="navyblue", lwd=3)
 }
--- R/plot.opt_prediction_object.R
+++ R/plot.opt_prediction_object.R
@@ -1,14 +1,13 @@
 #' @export
-#' @importFrom graphics points
 
 plot.opt_prediction_object = function(x, ...){
 
-  max.plot = round(max(x$result.table$num.trees_values)*1.1)
-  plot_seq = seq(0, max.plot, 1)
+  max_plot = round(max(x$result.table$num.trees_values)*1.1)
+  plot_seq = seq(0, max_plot, 1)
 
   plot(x$result.table$pred_stability ~ x$result.table$num.trees_values,
        main='Relationship between\n prediction stability and number of trees',
        ylab="Prediction stability", xlab="number of trees", ...)
-  points(TwoPLmodel(plot_seq, x$model.parameters[1,1], x$model.parameters[1,2]) ~ plot_seq,
-         type="l", col="navyblue", lwd=3)
+  graphics::lines(TwoPLmodel(plot_seq, x$model.parameters[1,1], x$model.parameters[1,2]) ~ plot_seq,
+         col="navyblue", lwd=3)
 }
